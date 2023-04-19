1721adfc5cf52a0ad9f6bf3e367d27e0b655f5de
Jimmy Gu <jimmyguu@outlook.com>
2023-04-19 21:54:01 +0800

    textarea demo

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <AwesomeInput v-model="address" @update:value="onChange" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AwesomeInput from "@/components/AwesomeInput.vue";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    AwesomeInput,
+  },
+  data() {
+    return {
+      address: "",
+    }
+  },
+  methods: {
+    onChange(newValue) {
+      newValue = newValue || "";
+      this.$nextTick(() => {
+        this.address = newValue.replace(/[^a-zA-Z]/g, "").toUpperCase();
+      });
+    },
+  },
 }
 </script>
 
---
+++ src/components/AwesomeInput.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="awesome-input">
+    <textarea v-model="valueInner" cols="30" rows="10"></textarea>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AwesomeInput",
+  model: {
+    prop: "value",
+    event: "update:value",
+  },
+  props: {
+    value: String,
+  },
+  data() {
+    return {};
+  },
+  computed: {
+    valueInner: {
+      get() {
+        return this.value;
+      },
+      set(newValue) {
+        this.$emit("update:value", newValue);
+      },
+    },
+  },
+  methods: {
+  },
+}
+</script>
+
+<style lang="css" scoped></style>
